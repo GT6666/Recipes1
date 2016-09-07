94a6d018b311824b83dd1823645af205caaf0903
FF <ff1003733488@163.com>
2016-09-07 11:42:01 +0800

    添加了图片 和颜色背景

--- TastyRecipes/src/main/java/com/project/wei/tastyrecipes/basepager/subclass/basepager/Cooking.java
+++ TastyRecipes/src/main/java/com/project/wei/tastyrecipes/basepager/subclass/basepager/Cooking.java
@@ -189,8 +189,8 @@ public class Cooking extends BasePager{
     //初始化网络图片缓存库
     private void initImageLoader(){
         //网络图片例子,结合常用的图片缓存库UIL,你可以根据自己需求自己换其他网络图片库
-        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
-                showImageForEmptyUri(R.drawable.ic_default_adimage)
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                //showImageForEmptyUri(R.drawable.ic_default_adimage)
                 .cacheInMemory(true).cacheOnDisk(true).build();
 
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
