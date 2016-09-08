4b5f69c736d1cdeea0238a55aaf7dfd73dd0caff
blackAndWhite7 <651600056@qq.com>
2016-09-08 22:39:13 +0800

    真是难

--- TastyRecipes/src/main/java/com/project/wei/tastyrecipes/bean/ChannelItem.java
+++ TastyRecipes/src/main/java/com/project/wei/tastyrecipes/bean/ChannelItem.java
@@ -1,12 +1,11 @@
 package com.project.wei.tastyrecipes.bean;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.io.Serializable;
 
 /**
  * ITEM的对应可序化队列属性
  *  */
-  public class ChannelItem implements Parcelable{
+  public class ChannelItem implements Serializable{
 
 //	private static final long serialVersionUID = -6465237897027410019L;
 
@@ -18,7 +17,60 @@ import android.os.Parcelable;
 
 	public int selected;
 
-	protected ChannelItem(Parcel in) {
+	public ChannelItem() {
+	}
+
+	public ChannelItem(int id, String name, int orderId, int selected) {
+		this.id = id;
+		this.name = name;
+		this.orderId = orderId;
+		this.selected = selected;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getSelected() {
+		return selected;
+	}
+
+	public void setSelected(int selected) {
+		this.selected = selected;
+	}
+
+	@Override
+	public String toString() {
+		return "ChannelItem{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", orderId=" + orderId +
+				", selected=" + selected +
+				'}';
+	}
+
+	/*	protected ChannelItem(Parcel in) {
 		id = in.readInt();
 		name = in.readString();
 		orderId = in.readInt();
@@ -95,5 +147,5 @@ import android.os.Parcelable;
 	public String toString() {
 		return "ChannelItem [id=" + this.id + ", name=" + this.name
 				+ ", selected=" + this.selected + "]";
-	}
+	}*/
 }
